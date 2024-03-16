ab203f3657856be9ef470a103a059c45a9fe37fc
Omar Osama <o.osama@youngcapital.nl>
2024-03-16 17:00:56 +0100

    - add rspec test - breakdown script to smaller services

---
+++ store_meta_data.rb
@@ -0,0 +1,42 @@
+require 'wavefile'
+require 'gyoku'
+require 'dry/monads'
+require_relative './models/wav_file'
+require 'fileutils'
+
+class StoreMetaData
+  include Dry::Monads[:result]
+
+  DEFAULT_OUTPUT_DIRECTORY = 'output'
+
+  attr_reader :xml_map, :output_directory
+
+  def initialize(attributes={})
+    @xml_map = attributes[:xml_map]
+    @output_directory = attributes[:output_directory] || DEFAULT_OUTPUT_DIRECTORY
+  end
+
+  def call
+    return Failure("missing argument xml_map") unless xml_map
+
+    return Failure("output directory doesn't exist") unless File.exist?(output_directory.to_s)
+
+    xml_map.each do |name, xml|
+      xml_filepath = get_xml_filepath(name)
+
+      FileUtils.mkdir_p(File.dirname(xml_filepath))
+
+      File.open(xml_filepath, 'w') do |file|
+        file << xml
+      end
+    end
+
+    Success("Successfully created #{xml_map.count} xml files")
+  end
+
+  private
+
+  def get_xml_filepath(name)
+    "#{output_directory}/#{Time.now.to_i}/#{name}.xml"
+  end
+end
